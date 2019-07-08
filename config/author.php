4f0d79fd8aca89ab909a893d669a73ddf868d722
Ferdous Anam <ferdous.anam@gmail.com>
2019-07-08 15:58:21 +0600

    first commit

---
+++ config/author.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Company/Author/Developer/Licence information of the application
+    |--------------------------------------------------------------------------
+    |
+    | All details about the author/developer/contact/licence of the application
+    |
+    | IMPORTANT: CHANGING ANY OF THIS INFORMATION WILL UNSTABILIZE THE APPLICATION
+    |
+    */
+
+    'vendor' => 'UrwaTech',
+    'vendor_url' => 'https://urwatech.com',
+    'email' => 'info@urwatech.com',
+    'app_version' => "2.14.3",
+    'lic1' => 'aHR0cHM6Ly9sLnVsdGltYXRlZm9zdGVycy5jb20vYXBpL3R5cGVfMQ==',
+    'pid' => 1,
+];
